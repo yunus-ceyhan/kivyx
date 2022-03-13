e26aaa9c72b347e27da20d339830241f8c237459
yunus-ceyhan <yunus.ceyhn@gmail.com>
2022-03-13 05:00:25 -0700

    font path

--- kivyx/icon.py
+++ kivyx/icon.py
@@ -1,13 +1,16 @@
 from kivy.lang import Builder
 from kivyx.label import XLabel
 from kivy.properties import StringProperty, ColorProperty
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'data/icon.ttf')
 
 Builder.load_string("""
 #:import x_icons kivyx.icon_def.x_icons
 #:import os os
 <XIcon>:
     text: x_icons[root.icon] if root.icon and root.icon in x_icons.keys() else ""
-    font_name: "./kivyx/data/icon.ttf" if os.path.isfile("./kivyx/data/icon.ttf") else "./data/icon.ttf"
+    font_name: root.font
     font_size: "24dp"
     size_hint: None, None
     size: self.font_size , self.font_size
@@ -19,6 +22,7 @@ Builder.load_string("""
 class XIcon(XLabel):
     icon = StringProperty()
     color = ColorProperty([0,0,0,1])
+    font = filename
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.color = self.txt_color
