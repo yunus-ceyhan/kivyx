9f8f0cb1638ef12e3eab021b6a40715ea737bdf3
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2022-05-22 23:42:12 -0700

    Create fonts.py

---
+++ kivyx/fonts.py
@@ -0,0 +1,40 @@
+from kivy.core.text import LabelBase
+
+from kivyx import fonts_path
+
+fonts = [
+    {
+        "name": "Roboto",
+        "fn_regular": fonts_path + "Roboto-Regular.ttf",
+        "fn_bold": fonts_path + "Roboto-Bold.ttf",
+        "fn_italic": fonts_path + "Roboto-Italic.ttf",
+        "fn_bolditalic": fonts_path + "Roboto-BoldItalic.ttf",
+    },
+    {
+        "name": "RobotoThin",
+        "fn_regular": fonts_path + "Roboto-Thin.ttf",
+        "fn_italic": fonts_path + "Roboto-ThinItalic.ttf",
+    },
+    {
+        "name": "RobotoLight",
+        "fn_regular": fonts_path + "Roboto-Light.ttf",
+        "fn_italic": fonts_path + "Roboto-LightItalic.ttf",
+    },
+    {
+        "name": "RobotoMedium",
+        "fn_regular": fonts_path + "Roboto-Medium.ttf",
+        "fn_italic": fonts_path + "Roboto-MediumItalic.ttf",
+    },
+    {
+        "name": "RobotoBlack",
+        "fn_regular": fonts_path + "Roboto-Black.ttf",
+        "fn_italic": fonts_path + "Roboto-BlackItalic.ttf",
+    },
+    {
+        "name": "Icons",
+        "fn_regular": fonts_path + "materialdesignicons-webfont.ttf",
+    },
+]
+
+for font in fonts:
+    LabelBase.register(**font)
