92c61a2b73bcef21039974c701d550c92d88f707
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2022-06-08 05:17:01 -0700

    Update icon.py

--- kivyx/icon.py
+++ kivyx/icon.py
@@ -3,14 +3,17 @@ from kivyx.label import XLabel
 from kivy.properties import StringProperty, ColorProperty
 import os
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'data/icon.ttf')
+material = os.path.join(dirname, 'data/icon.ttf')
+solid = os.path.join(dirname, 'data/Free-Solid-900.otf')
+brand = os.path.join(dirname, 'data/Brands-Regular-400.otf')
+regular = os.path.join(dirname, 'data/Free-Regular-400.otf')
 
 Builder.load_string("""
 #:import x_icons kivyx.icon_def.x_icons
 #:import os os
 <XIcon>:
     text: x_icons[root.icon] if root.icon and root.icon in x_icons.keys() else ""
-    font_name: root.font
+    font_name: root.s_font if self.text.startswith("s-") else root.b_font if self.text.startswith("s-") else root.r_font self.text.startswith("r-") else m_font
     font_size: "24dp"
     size_hint: None, None
     size: self.font_size , self.font_size
@@ -22,7 +25,10 @@ Builder.load_string("""
 class XIcon(XLabel):
     icon = StringProperty()
     color = ColorProperty([0,0,0,1])
-    font = filename
+    m_fonnt = material
+    s_font = solid
+    b_font = brand
+    r_font = regular
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.color = self.txt_color
