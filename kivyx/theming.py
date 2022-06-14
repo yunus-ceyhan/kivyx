cfb19466d9a45080544c75b0e28adc70cd318474
yunus-ceyhan <yunus.ceyhn@gmail.com>
2022-06-14 12:15:35 +0300

    color

--- kivyx/theming.py
+++ kivyx/theming.py
@@ -8,8 +8,6 @@ from kivy.core.window import Window
 class Theming(EventDispatcher):
     theme = StringProperty("Light")
     colorx = DictProperty()
-    paletx = DictProperty()
-    colors = DictProperty()
     bgr_color = ColorProperty()
     card_color = ColorProperty()
     primary_color = ColorProperty()
@@ -21,12 +19,38 @@ class Theming(EventDispatcher):
     shadow_color = ColorProperty([0.5,0.5,0.5,0.05])
     trans_color = ColorProperty([0.0,0.0,0.0,0.0])
     disabled_color = ColorProperty()
+    xcolors = {
+        "red" :   "#E36036",
+        "red2":   "#e74c3c",
+        "red3":   "#e74c3c",
+        "grey":   "#7f8c8d",
+        "green":  "#30C29E",
+        "green2": "#1abc9c",
+        "green3": "#16a085",
+        "green4": "#2ecc71",
+        "green5": "#27ae60",
+        "blue" :  "#4686EE",
+        "blue2": "#3498db",
+        "blue3": "#2980b9",
+        "yellow": "#FFD369",
+        "sun":    "#f1c40f",
+        "black" : "#222831",
+        "white": "#EEEEEE",
+        "orange": "#f39c12",
+        "carrot": "#e67e22",
+        "pumpkin": "#d35400",
+        "silver": "#bdc3c7",
+        "purple": "#9b59b6",
+        "purple2": "#8e44ad",
+        "asphalt": "#34495e",
+        "midnight": "#2c3e50",
+        "clouds": "#ecf0f1",
+        "trans": "#ffffff00"
+    }
 
     def __init__(self, **kwargs):
         super(Theming, self).__init__(**kwargs)
-        self.colors =  x_colors["Palet"]
         self.theme = App.get_running_app().theme_style
-        self.paletx = x_colors["Palet"]
         if self.theme == "Dark":
             self.colorx = x_colors["Dark"]           
         else:
