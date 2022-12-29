6f648ec232202db9e55b0260c1804f14404b19f0
yunus-ceyhan <yunus.ceyhn@gmail.com>
2022-12-29 16:11:28 +0300

    shadows

--- kivyx/segmentcontrol.py
+++ kivyx/segmentcontrol.py
@@ -70,7 +70,7 @@ Builder.load_string("""
         width: s.children[0].width if len(s.children) > 0 else 0
         pos: root.current_button.pos if root.current_button != None else [self.pos[0], xsc.pos[1] + (xsc.height -self.height)/2 ]
         bg_color: root.item_color
-        elevation: 0.2
+        elevation: 0.3
         shadow_distance: - dp(1)
         shadow_y: 0
         shadow_blur: dp(4)
