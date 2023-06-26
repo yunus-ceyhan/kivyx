eece9b00fd2782f89dd80fcc49d612b21d303649
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2023-06-25 20:59:45 -0700

    Update shadow.py

--- kivyx/shadow.py
+++ kivyx/shadow.py
@@ -6,16 +6,6 @@ Builder.load_string("""
 <XShadow>:
     size_hint_y: None
     height: dp(15)
-    canvas.before:
-        Color:
-            rgba: (0, 0, 0, root.elevation)
-        BoxShadow:
-            pos: self.pos
-            size: self.size
-            offset: 0, - dp(1)
-            spread_radius: - dp(4), - dp(4)
-            border_radius: [0,0,0,0]
-            blur_radius: dp(10)
 """)
 
 class XShadow(BoxLayout):
